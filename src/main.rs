069e5bee98263e9e0cd637ba36b0d5eb1a30397e
pierluigigiancola <pierluigi.giancola@gmail.com>
2023-04-29 19:26:55 +0200

    chore: counter example

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use leptos::*;
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+
+    view! { cx,
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+        >
+            "Click me: "
+            {move || count.get()}
+        </button>
+    }
+}
+
+fn main() {
+    mount_to_body(|cx| view! { cx, <App/> })
+}
