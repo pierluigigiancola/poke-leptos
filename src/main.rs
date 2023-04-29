12a835f901e6ac5b6d43ceabb67ea29ba2db6e53
pierluigigiancola <pierluigi.giancola@gmail.com>
2023-04-29 19:52:07 +0200

    chore: passing count directly into an attribute

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,23 @@ fn App(cx: Scope) -> impl IntoView {
     let (count, set_count) = create_signal(cx, 0);
 
     view! { cx,
-        <button
-            style= move || format!("background-color: {}", if count() % 2 == 0  {"red"} else {"blue"})
-            class:red=move || count() % 2 == 0
-            class:blue=move || count() % 2 == 1
-            on:click=move |_| {
-                set_count.update(|n| *n += 1);
-            }
-        >
-            "Click me: "
-            {move || count.get()}
-        </button>
-    }
+            <button
+                style= move || format!("background-color: {}", if count() % 2 == 0  {"red"} else {"blue"})
+                class:red=move || count() % 2 == 0
+                class:blue=move || count() % 2 == 1
+                on:click=move |_| {
+                    set_count.update(|n| *n += 1);
+                }
+            >
+                "Click me: "
+                {move || count.get()}
+            </button>
+            <progress
+        max="50"
+        // signals are functions, so this <=> `move || count.get()`
+        value=count
+    />
+        }
 }
 
 fn main() {
