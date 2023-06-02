a71c7c9feda2a1e3f5339a261dbd4f49a8f622e9
pierluigigiancola <pierluigi.giancola@gmail.com>
2023-06-02 10:40:12 +0200

    feat: progress bar component

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
 use leptos::*;
 
 #[component]
+fn ProgressBar(cx: Scope, progress: ReadSignal<i32>) -> impl IntoView {
+    view! { cx,
+        <progress
+            max="50"
+            value=progress
+        />
+    }
+}
+
+#[component]
 fn App(cx: Scope) -> impl IntoView {
     let (count, set_count) = create_signal(cx, 0);
 
     view! { cx,
-            <button
-                style= move || format!("background-color: {}", if count() % 2 == 0  {"red"} else {"blue"})
-                class:red=move || count() % 2 == 0
-                class:blue=move || count() % 2 == 1
-                on:click=move |_| {
-                    set_count.update(|n| *n += 1);
-                }
-            >
-                "Click me: "
-                {move || count.get()}
-            </button>
-            <progress
-        max="50"
-        // signals are functions, so this <=> `move || count.get()`
-        value=count
-    />
-        }
+        <button
+            style= move || format!("background-color: {}", if count() % 2 == 0  {"red"} else {"blue"})
+            class:red=move || count() % 2 == 0
+            class:blue=move || count() % 2 == 1
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+        >
+            "Click me: "
+            {move || count.get()}
+        </button>
+        <ProgressBar progress={count} />
+    }
 }
 
 fn main() {
